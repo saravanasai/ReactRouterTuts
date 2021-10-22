e36a168a0384f5d4b02e976e3dfb54a4f185013a
Saravana Sai <saravanasai002@gmail.com>
2021-10-22 21:20:48 +0530

    fisnidhed understading

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
    
 
   return (
-    <div style={{ display: "flex", height: "100vh", alignItems: "stretch" }}>
-      <div style={{ flex: 0.3, backgroundColor: "#f2f2f2" }}>route menu</div>
-      <div> <RenderRoutes routes={ROUTES} /></div>
-    </div>
+      <div>
+         <Header />
+         <RenderRoutes routes={ROUTES} />
+     </div>
   );
 }
 
--- src/Routes/route.js
+++ src/Routes/route.js
@@ -1,28 +1,41 @@
 import React from "react"
 import { Route, Switch } from "react-router";
+import Home from "../components/Home";
 
 const ROUTES = [
-    { path: "/", key: "ROOT", exact: true, component: () => <h1>Log in</h1> },
-    {
-      path: "/app",
-      key: "APP",
-      component: () => RenderRoutes,
-      routes: [
-        {
-          path: "/app",
-          key: "APP_ROOT",
-          exact: true,
-          component: () => <h1>App Index</h1>,
-        },
-        {
-          path: "/app/page",
-          key: "APP_PAGE",
-          exact: true,
-          component: () => <h1>App Page</h1>,
-        },
-      ],
-    },
-  ];
+  { path: "/", key: "ROOT", exact: true, component: () => <h1>Log in</h1> },
+  {
+    path: "/app",
+    key: "APP",
+    component: RenderRoutes, // here's the update
+    routes: [
+      {
+        path: "/app",
+        key: "APP_ROOT",
+        exact: true,
+        component: () => <h1>App Index</h1>,
+      },
+      {
+        path: "/app/page",
+        key: "APP_PAGE",
+        exact: true,
+        component: () => <h1>App Page</h1>,
+      },
+      {
+        path: "/app/home",
+        key: "APP_HOME",
+        exact: true,
+        component: () => <Home />,
+      },
+    ],
+  },
+  {
+    path:"/test",
+    exact:true,
+    key:"TEST",
+    component:()=>(<h2>TEST ROUTE ON OWN</h2>)
+  }
+];
   
   export default ROUTES;
 
